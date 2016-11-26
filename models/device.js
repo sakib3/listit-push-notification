4630c0d38c1170b5f20d55dde996c0cf5d7bfa10
sakib3 <sakib3@gmail.com>
2016-11-27 00:26:47 +0100

    mongodb url

--- models/device.js
+++ models/device.js
@@ -3,13 +3,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var deviceSchema = mongoose.Schema({
-  
+
 	deviceName 		: String,
 	deviceId		: String,
 	registrationId	: String
 
 });
 
-mongoose.connect('mongodb://localhost:27017/node-android-push');
+//mongoose.connect('mongodb://localhost:27017/node-android-push');
+mongoose.connect('mongodb://'+process.env.DB_USER+':'+process.env.DB_PASSWORD+'@mongodb.cloudno.de:27017/node-android-push');
 
 module.exports = mongoose.model('device', deviceSchema);
