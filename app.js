ef5ac1880ef4dd43dc5890f055959ffee025360f
sakib3 <sakib3@gmail.com>
2016-11-26 23:29:12 +0100

    little modify

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var port = process.env.PORT || 8080;
+var port = process.env.app_port || 8080;
 var logger = require('morgan');
 var io = require('socket.io');
 
-//app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 
